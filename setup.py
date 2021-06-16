f55c676427a40b4ebcc90adc1ff46ed6fc6797ef
Aaron LG <62504491+Aaron-AALG@users.noreply.github.com>
2021-06-16 12:25:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ setup(
     description = 'Unweighted TOPSIS method',
     long_description=long_description,
     license = 'MIT',
+    url='https://github.com/Aaron-AALG/uwTOPSIS',
+    download_url = 'https://github.com/Aaron-AALG/uwTOPSIS/releases/tag/uwTOPSIS'
     install_requires=['pandas >= 1.2.4',
                       'numpy >= 1.19',
                       'scipy >= 1.6.3'],
